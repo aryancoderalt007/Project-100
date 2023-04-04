8c19ad451059bae6868f7e5f3df58b11aecf90c4
aryancoderalt007 <128900253+aryancoderalt007@users.noreply.github.com>
2023-04-04 12:36:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -71,13 +71,13 @@ function take_snapshot(){
 
     Webcam.snap(function(data_uri){
         if(img_id== "selfie1"){
-            document.getElementById("results1").innerHTML = '<img id="selfie1" src="' + data_uri + '"/>';
+            document.getElementById("result1").innerHTML = '<img id="selfie1" src="' + data_uri + '"/>';
         }
         if(img_id== "selfie2"){
-            document.getElementById("results1").innerHTML = '<img id="selfie2" src="' + data_uri + '"/>';
+            document.getElementById("result2").innerHTML = '<img id="selfie2" src="' + data_uri + '"/>';
         }
         if(img_id== "selfie3"){
-            document.getElementById("results1").innerHTML = '<img id="selfie3" src="' + data_uri + '"/>';
+            document.getElementById("result3").innerHTML = '<img id="selfie3" src="' + data_uri + '"/>';
         }
     });
 }
